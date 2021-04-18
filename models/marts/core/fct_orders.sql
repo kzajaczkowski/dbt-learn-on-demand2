4d804498d131eec04872d44eaac0375bf7b34673
Krzysztof Zajączkowski <krzysztof.lukasz.zajaczkowski@gmail.com>
2021-04-18 20:40:03 +0000

    finał adjustments for model module

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,1 +1,26 @@
-SELECT 1
+with payments as (
+
+    select * from {{ ref('stg_payments')}}  
+
+),
+
+orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+successful_payments as (
+
+    select *
+    from payments
+    where status = 'success'
+)
+
+select orders.customer_id
+   , orders.order_id
+   , orders.order_date
+   , successful_payments.amount
+from orders
+left join successful_payments
+using (order_id)
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,3 +1,4 @@
-select orderid
-   , amount
+select orderid as order_id
+   , amount / 100 as amount
+   , status
 from raw.stripe.payment
