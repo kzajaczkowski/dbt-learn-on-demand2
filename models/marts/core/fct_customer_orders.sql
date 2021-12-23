00fcae3d29ab56841e5b3689b9865ec31d79aaae
Krzysztof Zajączkowski <krzysztof.lukasz.zajaczkowski@gmail.com>
2021-12-23 08:56:15 +0000

    Nothing really

--- models/marts/core/fct_customer_orders.sql
+++ models/marts/core/fct_customer_orders.sql
@@ -40,7 +40,14 @@ order_values_joined as (
             p.total_amount_paid,
             p.payment_finalized_date,
             c.customer_first_name,
-            c.customer_last_name
+            c.customer_last_name--,
+            -- min(orders.order_placed_at) over (partition by customer_id) as first_customer_order,
+            -- max(orders.order_placed_at) over (partition by customer_id) as last_customer_order,
+            -- count(orders.order_id) over (partition by customer_id) as number_of_orders,
+            -- case when first_customer_order = orders.order_placed_at
+            --     then 'new'
+            --     else 'return' end as nvsr
+
     from orders
     left join order_totals p
     using (order_id)
@@ -52,8 +59,8 @@ order_values_joined as (
 customer_orders as (
 
     select c.customer_id,
-        min(order_placed_at) as fdos,
-        max(order_placed_at) as most_recent_order_date,
+        min(order_placed_at) as first_customer_order,
+        max(order_placed_at) as last_customer_order,
         count(orders.order_id) as number_of_orders
     from customers c 
     left join orders
@@ -79,14 +86,11 @@ final as (
 
     select
         p.*,
-        row_number() over (order by p.order_id) as transaction_seq,
-        row_number() over (partition by customer_id order by p.order_id)
-            as customer_sales_seq,
-        case when c.fdos = p.order_placed_at
+        case when c.first_customer_order = p.order_placed_at
             then 'new'
             else 'return' end as nvsr,
         x.customer_lifetime_value,
-        c.fdos
+        c.first_customer_order as fdos
     from order_values_joined p
     left join customer_orders c
     using (customer_id)
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -11,6 +11,9 @@ transformed as (
 
     select
         orders.id as order_id,
+        row_number() over (order by orders.id) as transaction_seq,
+        row_number() over (partition by orders.user_id order by orders.id)
+            as customer_sales_seq,
         orders.user_id	as customer_id,
         orders.order_date as order_placed_at,
         orders.status as order_status
